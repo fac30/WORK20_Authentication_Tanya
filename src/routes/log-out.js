0ab9a9554a032acdb86d59a73eea655ca928eb2a
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-10-21 14:53:13 +0100

    fix the issue with log-out challenge

--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,23 +1,10 @@
 const { removeSession } = require('../model/session.js'); 
 
-async function post(req, res) {
-  try {
-    const sid = req.signedCookies.sid || req.cookies.sid;
-    if (sid) {
-      await removeSession(sid);
+function post(req, res) {
+    const sid = req.signedCookies.sid;
+      removeSession(sid);
       res.clearCookie('sid');
       return res.redirect('/');
-    } else {
-      return res.redirect('/');
-    }
-  } catch (err) {
-    console.error('Error during logout:', err);
-    return res.status(500).send('Failed to log out');
-  }
-  // const sid = req.signedCookie.sid;
-  // removeSession(sid);
-  // res.clearCookie('sid');
-  // res.redirect('/');
   /**
    * [1] Get the session ID from the cookie
    * [2] Remove that session from the DB
